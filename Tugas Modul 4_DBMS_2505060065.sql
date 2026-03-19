98c15d86c745ad0d49bb37adb116925ae2bda724
Aghnia Azka <dhyazka12@gmail.com>
2026-03-19 20:13:21 +0700

    tugas modul 4

---
+++ Tugas Modul 4_DBMS_2505060065.sql
@@ -0,0 +1,107 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: kampus
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+DROP TABLE IF EXISTS `mahasiswa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mahasiswa` (
+  `npm` varchar(15) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  PRIMARY KEY (`npm`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+LOCK TABLES `mahasiswa` WRITE;
+/*!40000 ALTER TABLE `mahasiswa` DISABLE KEYS */;
+INSERT INTO `mahasiswa` VALUES ('255030032','Aghnia Azka','Teknologi Informasi'),('255030033','Leonel Rodhes','Teknik Sipil'),('255030034','Atthala Rayhan','Teknik Sipil'),('255030035','Nayla Husna','Teknologi Informasi'),('255030036','Jiwadipta Abireksa','Teknologi Informasi'),('255030037','Melladona Takkiya','Teknik Sipil');
+/*!40000 ALTER TABLE `mahasiswa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mata_kuliah`
+--
+
+DROP TABLE IF EXISTS `mata_kuliah`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mata_kuliah` (
+  `kode_mk` varchar(10) NOT NULL,
+  `nama_mk` varchar(100) NOT NULL,
+  `sks` int(11) NOT NULL,
+  PRIMARY KEY (`kode_mk`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mata_kuliah`
+--
+
+LOCK TABLES `mata_kuliah` WRITE;
+/*!40000 ALTER TABLE `mata_kuliah` DISABLE KEYS */;
+INSERT INTO `mata_kuliah` VALUES ('MK001','Paradigma Sistem',2),('MK002','Basis Data',3),('MK003','Mekanika Fluida',3),('MK004','Perancangan Transportasi',2);
+/*!40000 ALTER TABLE `mata_kuliah` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nilai`
+--
+
+DROP TABLE IF EXISTS `nilai`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `nilai` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `npm` varchar(15) NOT NULL,
+  `kode_mk` varchar(10) NOT NULL,
+  `nilai` decimal(5,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `npm` (`npm`),
+  KEY `kode_mk` (`kode_mk`),
+  CONSTRAINT `nilai_ibfk_1` FOREIGN KEY (`npm`) REFERENCES `mahasiswa` (`npm`),
+  CONSTRAINT `nilai_ibfk_2` FOREIGN KEY (`kode_mk`) REFERENCES `mata_kuliah` (`kode_mk`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nilai`
+--
+
+LOCK TABLES `nilai` WRITE;
+/*!40000 ALTER TABLE `nilai` DISABLE KEYS */;
+INSERT INTO `nilai` VALUES (1,'255030032','MK001',82.00),(2,'255030033','MK003',75.00),(3,'255030034','MK004',93.00),(4,'255030035','MK002',80.00),(5,'255030036','MK001',90.00),(6,'255030037','MK003',78.00);
+/*!40000 ALTER TABLE `nilai` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-19 19:41:43
